466a6d0c45b5896dd4a4c5d7bf2415a93df46ebe
Emile Clark-Boman <nordicdsgn@gmail.com>
2020-06-14 15:38:49 +1000

    relocate static and template folders

--- src/main.py
+++ src/main.py
@@ -2,13 +2,25 @@
 from flask import Flask
 from flask import render_template as render
 
+# configure flask app
 app = Flask('Refract')
+# change folder locations
+app.template_folder = 'Core/Templates'
+app.static_folder = 'Core/Static'
 
+# ROUTES
 @app.route('/')
 @app.route('/home')
 def index():
     return render("index.html")
 
+@app.route('/portrait')
+def portrait():
+    return render("portrait.html")
+
+@app.route('/loading')
+def loading():
+    return render('loading.html')
 
 def main():
     # run on localhost
